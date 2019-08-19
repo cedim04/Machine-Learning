46442b4e18e01685d3481ec8f3a649b0e9241d50
cedim04 <54278080+cedim04@users.noreply.github.com>
2019-08-19 14:00:06 -0400

    finished simple chatbot

--- simplechat/simplechat.py
+++ simplechat/simplechat.py
@@ -1,3 +1,5 @@
+import re
+
 prompts = {
     "what": "What is a airplane?",
     "where": "Where can they be flown?",
@@ -7,7 +9,7 @@ prompts = {
 
 }
 
-responces = {
+responses = {
     "what": "An airplane or aeroplane (informally plane) is a powered, fixed-wing aircraft that is propelled forward by thrust from a jet engine, propeller or rocket engine.",
     "where": "Any airport that has the proper means to support a aircraft of that side",
     "who": " Worldwide, commercial aviation transports more than four billion passengers annually on airliners[1] and transports more than 200 billion tonne-kilometres[2] of cargo annually",
@@ -22,8 +24,45 @@ responces = {
 
 
 def processInput(userInput):
-    #return"Hello"
-    pass 
+    #if its not whitespace or alfa numeric, then get rid of it
+    userInput = re.sub(r'[^\w\s]', '', userInput)
+
+    words = userInput.split(" ")
+
+    matchingKeys = []
+
+    for word in words:
+        if word in responses.keys():
+            matchingKeys.append(word)
+
+    if len(matchingKeys) == 0:
+        return "I haven't learned that quite yet!"
+    elif len(matchingKeys) == 1:
+        return responses[matchingKeys[0]]
+    else:
+        print("I'm not sure what you mean. Did you mean: ")
+        index = 1
+
+
+        for key in matchingKeys:
+            print(str(index) + ": " + prompts[key])
+            index += 1
+
+        valid = False
+
+        while not valid:
+            selected = int(input("#: "))
+
+            if selected <= len(matchingKeys) and selected > 0:
+                valid = True
+            else:
+                print("PLease enter one of the above")
+        return responses[matchingKeys[selected - 1]]
+
+
+
+    
+     
 
 def main():
     print("Welcome to Airplane Facts! I can talk to you about airplanes\n")
