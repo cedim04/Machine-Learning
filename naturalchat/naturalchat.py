8a95d1f48949924edd105e84ba5319ae42a7fe23
cedim04 <54278080+cedim04@users.noreply.github.com>
2019-08-20 12:06:37 -0400

    natural chat

---
+++ naturalchat/naturalchat.py
@@ -0,0 +1,37 @@
+import nltk
+import numpy as np
+import random
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+f = open('info.txt', 'r')
+raw = f.read().lower()
+
+scentence_tokens = nltk.sent_tokenize(raw)
+
+lemmer = nltk.stem.WordNetLemmatizer()
+
+
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "hey", "what's up", "what's popping")
+GREETING_RESPONSE = ("hi", "how are you doing", "i'm sleeping", "thank you for waisting your time with me today", "how are the kids")
+
+def greeting(sentence):
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSE)
+
+print(greeting("Hello"))
+
+
+    
+
