9db1258d7f772d9fbda9c60033fa3efbb2a4948d
cedim04 <54278080+cedim04@users.noreply.github.com>
2019-08-20 11:12:37 -0400

    Update facetracking.py

--- opencv-face-tracking/facetracking.py
+++ opencv-face-tracking/facetracking.py
@@ -1,31 +1,56 @@
 import cv2
 import time
+import random
+
+CAPTURE_WIDTH = 1280
+CAPTURE_HIGHT = 720
+
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 320)
-cap.set(4, 240)
+cap.set(3, CAPTURE_WIDTH)
+cap.set(4, CAPTURE_HIGHT)
 
 classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 time.sleep(1)
 
+points = 0
+hitTarget = True
+targetX = 0
+targetY = 0
+targetW = 50
+
+
 while(True):
     _, img = cap.read()
+    img = cv2.flip(img, 1)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces = classifier.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (0, 200, 0), 5)
+        cv2.rectangle(img, (x,y), (x+w, y+h), (47, 184, 2), 5)
+
+        if (x <= targetX <= x + w) and (y<= targetY <= y +h):
+            hitTarget = True
+            points += 1
+
+
+
+    if hitTarget:
+        targetX = random.randint(targetW, CAPTURE_WIDTH - targetW)
+        targetY = random.randint(targetW, CAPTURE_HIGHT - targetW)
+        hitTarget = False
+
+    cv2.rectangle(img, (targetX, targetY), (targetX + targetW, targetY + targetW), (0, 0, 255), 20)
+
+    cv2.putText(img, "Points: " + str(points), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 87), 1)
+
 
     cv2.imshow('Face Tracking', img)
 
-    if cv2.waitKey(1) & 0xF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         cap.release()
         cv2.destroyAllWindows()
         break
-
-
-
-    
