ea765ef0b031e844eacae6dd982135f18ca643f0
cedim04 <54278080+cedim04@users.noreply.github.com>
2019-08-20 13:53:45 -0400

    Update naturalchat.py

--- naturalchat/naturalchat.py
+++ naturalchat/naturalchat.py
@@ -2,13 +2,14 @@ import nltk
 import numpy as np
 import random
 import string
+import re 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 f = open('info.txt', 'r')
 raw = f.read()
 
-scentence_tokens = nltk.sent_tokenize(raw)
+sentence_tokens = nltk.sent_tokenize(raw)
 
 lemmer = nltk.stem.WordNetLemmatizer()
 
@@ -26,11 +27,43 @@ GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "hey", "what's up", "what'
 GREETING_RESPONSE = ("hi", "how are you doing", "i'm sleeping", "thank you for waisting your time with me today", "how are the kids")
 
 def greeting(sentence):
+    sentence = re.sub(r'[^\w\s]', '', sentence)
     for word in sentence.split():
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSE)
 
-print(greeting("Hello"))
+def response(userInput):
+    sentence_tokens.append(userInput)
+    TfidVec = TfidfVectorizer(tokenizer=LemNormalize)
+    tfidf = TfidVec.fit_transform(sentence_tokens)
+
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    flat = vals.flatten()
+    idx = flat.argsort()[-2]
+
+    flat.sort()
+
+
+    return None 
+
+
+print("ROBOTBOT: My name is RobotBot. I will answer queries about airplanes. If you want to exit type bye")
+name = input("What's your name?")
+while True:
+    userInput = input(name + ": ").lower()
+    print("ROBOTBOT: ", end="")
+    if userInput != 'bye':
+        if greeting(userInput) is not None:
+            print(greeting(userInput))
+        else:
+            print(response(userInput))
+            sentence_tokens.remove(userInput)
+    else:
+        print("Bye!")
+        break 
+
+
+
 
 
 
