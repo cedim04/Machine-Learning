c7d93d9e5402e185bf061d0fa01094d0610dfec0
cedim04 <54278080+cedim04@users.noreply.github.com>
2019-08-21 12:06:52 -0400

    fly evolution

--- fly-evolution/sketch.js
+++ fly-evolution/sketch.js
@@ -6,32 +6,20 @@ const MUTATION_RATE = 0.1; //WHAT IS THE RATE AT WHICH FLIES MUTATE
 
 let count = 0;
 
-let fly, food, wall;
-
-
-
 
 function setup() {
-    console.log("Hello");
+    console.log('Hello');
 
     createCanvas(640, 480);
 
-    fly = new Fly(LIFE_SPAN);
-    food = new Food(width/2, 50, 30);
-    wall = new Wall(width/2, height - height/3, 300, 30);
 
-    
+    population = new Population(LIFE_SPAN, POP_SIZE, REWARD_MULTI, PUNISH_DIV)
+
 }
 
 function draw() {
     background(56, 88, 120);
-    
-    fly.update(count);
-    fly.show();
-   
-    food.show();
-
-    wall.show();
+    population.run(count)
 
 
     count ++;
